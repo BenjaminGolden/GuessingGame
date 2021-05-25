2646c04f02c1e5b25d2c5079f2a5e2efd39cd179
Benjamin Golden <b.j.golden@outlook.com>
2021-05-25 11:29:14 -0500

    phase 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿using System;
 
-
+//phase 1
 Console.WriteLine("Guess the secret number");
 Console.Write("enter your guess > ");
 string input = Console.ReadLine();
 int parsedInput = int.Parse(input);
-Console.Write("your guess = ");
-Console.WriteLine(input);
+// Console.Write("your guess = ");
+// Console.WriteLine(input);
+
+//phase 2
+int theNumber = 42;
+if (parsedInput == theNumber)
+{
+    Console.WriteLine("You're amazing!");
+}
+else
+{
+    Console.WriteLine("please guess again");
+}
