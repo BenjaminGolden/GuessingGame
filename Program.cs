7ba127a96a883727284bc4b0d4223ac2f79bfa82
Benjamin Golden <b.j.golden@outlook.com>
2021-05-25 11:19:31 -0500

    phase 1 complete

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+
+Console.WriteLine("Guess the secret number");
+Console.Write("enter your guess > ");
+string input = Console.ReadLine();
+int parsedInput = int.Parse(input);
+Console.Write("your guess = ");
+Console.WriteLine(input);
