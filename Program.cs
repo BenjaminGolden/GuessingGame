1e3135bbd7aefe357b947a35527e8e981e8ae8bb
Benjamin Golden <b.j.golden@outlook.com>
2021-05-25 12:05:21 -0500

    phase 3

--- Program.cs
+++ Program.cs
@@ -1,20 +1,37 @@
 ﻿using System;
 
+Main();
+void Main()
+{
 //phase 1
-Console.WriteLine("Guess the secret number");
-Console.Write("enter your guess > ");
-string input = Console.ReadLine();
-int parsedInput = int.Parse(input);
-// Console.Write("your guess = ");
-// Console.WriteLine(input);
 
-//phase 2
-int theNumber = 42;
-if (parsedInput == theNumber)
-{
-    Console.WriteLine("You're amazing!");
-}
-else
-{
-    Console.WriteLine("please guess again");
-}
+    Console.WriteLine("Guess the secret number");
+    Console.Write("enter your guess > ");
+    // Console.Write("your guess = ");
+    // Console.WriteLine(input);
+
+
+    //phase 3
+    int theNumber = 42;
+
+    for (int i = 0; i < 4; i++)
+    {
+    string input = Console.ReadLine();
+    int parsedInput = int.Parse(input);
+    //phase 2
+        if (parsedInput != theNumber && i < 3)
+        {
+            Console.Write("Nope! Try again? > ");
+            
+        }
+        else if (i == 3)
+        {
+            Console.WriteLine("out of attempts");
+        }
+        else 
+        {
+            Console.WriteLine("You're amazing!");
+        }
+
+    };
+};
